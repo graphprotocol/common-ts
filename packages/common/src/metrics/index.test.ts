7080791165f9dac1ec41bd6b160bd53a66ca004b
Jannis Pohlmann <jannis@thegraph.com>
2020-02-12 14:04:54 +0100

    Implement and test metrics server

--- packages/common/src/metrics/index.test.ts
+++ packages/common/src/metrics/index.test.ts
@@ -1,8 +1,47 @@
-import { createMetrics } from '.'
+import { Registry } from 'prom-client'
+import express, { Express } from 'express'
+import { createMetrics, createMetricsServer } from '.'
+import request from 'supertest'
+import { createLogger } from '../logging'
 
 describe('Metrics', () => {
   test('Create metrics', () => {
     let metrics = createMetrics()
-    expect(metrics.Counter).toBeDefined()
+    expect(metrics.client).toBeDefined()
+    expect(metrics.registry).toBeInstanceOf(Registry)
+  })
+
+  test('Serve metrics', async () => {
+    let { client, registry } = createMetrics()
+    let logger = createLogger({ appName: 'test' })
+    let server = createMetricsServer({ logger, registry })
+
+    try {
+      // Create two metrics for testing
+      let counter = new client.Counter({
+        name: 'counter',
+        help: 'counter help',
+        registers: [registry],
+      })
+      let gauge = new client.Gauge({
+        name: 'gauge',
+        help: 'gauge help',
+        registers: [registry],
+      })
+
+      counter.inc()
+      counter.inc()
+      gauge.set(100)
+
+      // Verify that the registered metrics are served at `/`
+      let response = await request(server)
+        .get('/')
+        .send()
+      expect(response.status).toEqual(200)
+      expect(response.text).toMatch(/counter 2/)
+      expect(response.text).toMatch(/gauge 100/)
+    } finally {
+      server.close()
+    }
   })
 })
--- packages/common/src/metrics/index.ts
+++ packages/common/src/metrics/index.ts
@@ -1,8 +1,36 @@
-import prometheus, { collectDefaultMetrics } from 'prom-client'
+import prometheus, { collectDefaultMetrics, Registry } from 'prom-client'
+import express from 'express'
+import { Server } from 'net'
+import { Logger } from '..'
 
-export const createMetrics = () => {
+export interface Metrics {
+  client: any
+  registry: Registry
+}
+
+export const createMetrics = (): Metrics => {
   // Probe default metrics (memory, file descriptors etc.) every 5s
   collectDefaultMetrics({ timeout: 5000 })
+  return { client: prometheus, registry: prometheus.register }
+}
 
-  return prometheus
+export interface MetricsServerOptions {
+  logger: Logger
+  registry: Registry
+}
+
+export const createMetricsServer = (options: MetricsServerOptions): Server => {
+  let logger = options.logger.child({ component: 'MetricsServer' })
+
+  let app = express()
+
+  app.get('/', (_, res) => {
+    res.status(200).send(options.registry.metrics())
+  })
+
+  let server = app.listen(7300, () => {
+    logger.debug('Listening on port 7300')
+  })
+
+  return server
 }
