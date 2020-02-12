1eb3e3957410ab4d0f41e6d8d837d43dd3110a10
Rahul Sethuram <rahul@Rahuls-MacBook-Pro.local>
2020-02-12 17:03:47 +0100

    Add store implementation

--- packages/common/src/state-channels/store.ts
+++ packages/common/src/state-channels/store.ts
@@ -1,7 +1,9 @@
 import * as Sequelize from 'sequelize'
 import { Store, StorePair } from '@connext/types'
 
-class Record extends Sequelize.Model {
+export class Record extends Sequelize.Model {
+  public path!: string
+  public value!: any
   static initialize(sequelize: Sequelize.Sequelize) {
     Record.init(
       {
@@ -29,11 +31,54 @@ export class SequelizeConnextStore implements Store {
   }
 
   async set(pairs: StorePair[], shouldBackup: boolean): Promise<void> {
-    throw 'Unimplemented'
+    for (const pair of pairs) {
+      // Wrapping the value into an object is necessary for Postgres bc the JSON column breaks
+      // if you use anything other than JSON (i.e. a raw string).
+      // In some cases, the cf core code is inserting strings as values instead of objects :(
+      const record = Record.build({ path: pair.path, value: { [pair.path]: pair.value } })
+      await record.save()
+    }
   }
 
   async get(path: string): Promise<any> {
-    throw 'Unimplemented'
+    let res: any
+    // special case for certain paths
+    if (path.endsWith('channel') || path.endsWith('appInstanceIdToProposedAppInstance')) {
+      res = await Record.findAll({
+        where: {
+          path: {
+            [Sequelize.Op.like]: `%${path}%`,
+          },
+        },
+      })
+      const nestedRecords = res.map((record: Record) => {
+        const existingKey = Object.keys(record.value)[0]
+        const leafKey = existingKey.split('/').pop()!
+        const nestedValue = record.value[existingKey]
+        delete record.value[existingKey]
+        record.value[leafKey] = nestedValue
+        return record.value
+      })
+      const records: { [key: string]: any } = {}
+      nestedRecords.forEach((record: any): void => {
+        const key = Object.keys(record)[0]
+        const value = Object.values(record)[0]
+        if (value !== null) {
+          records[key] = value
+        }
+      })
+      return records
+    }
+
+    res = await Record.findOne({
+      where: {
+        path,
+      },
+    })
+    if (!res) {
+      return undefined
+    }
+    return res.value[path]
   }
 
   async restore(): Promise<StorePair[]> {
