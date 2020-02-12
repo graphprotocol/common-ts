6153732a156911b72164a2f262973d7755409617
Jannis Pohlmann <jannis@thegraph.com>
2020-02-12 11:47:44 +0100

    Fully initialize client in createStateChannel()

--- packages/common/src/state-channels/index.ts
+++ packages/common/src/state-channels/index.ts
@@ -1,18 +1,31 @@
 import * as connext from '@connext/client'
 import { Sequelize } from 'sequelize'
-import { Store } from '@connext/types'
+import { Store, CF_PATH } from '@connext/types'
+import { HDNode } from 'ethers/utils'
+import { SequelizeConnextStore } from './store'
 
 interface StateChannelOptions {
   network: 'rinkeby'
-  db: Sequelize
+  sequelize: Sequelize
+  mnemonic: string
 }
 
 export const createStateChannel = async (options: StateChannelOptions) => {
-  // TODO: Create Sequelize-based store
-  // TODO: Define key generation
+  // Create Sequelize-based store
+  let store = new SequelizeConnextStore(options.sequelize)
+
+  // Create in-memory HDWallet from the mnemonic
+  const hdNode = HDNode.fromMnemonic(options.mnemonic).derivePath(CF_PATH)
+
+  // Obtain extended public key from the HDWallet
+  const xpub = hdNode.neuter().extendedKey
+
+  // Key derivation function
+  const keyGen = async (index: string) => hdNode.derivePath(index).privateKey
 
   return await connext.connect(options.network, {
-    store: undefined,
-    keyGen: undefined,
+    store,
+    keyGen,
+    xpub,
   })
 }
