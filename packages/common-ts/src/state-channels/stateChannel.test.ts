2d4586d04593860b5e0c8ba2912f24a924587e80
Rahul Sethuram <rahul@Rahuls-MacBook-Pro.local>
2020-04-02 20:14:06 +0200

    Add optional test config for connext client

---
+++ packages/common-ts/src/state-channels/stateChannel.test.ts
@@ -0,0 +1,27 @@
+import { Sequelize } from 'sequelize/types'
+import { connect as dbConnect } from '../database'
+import { createStateChannel } from '.'
+import { Wallet } from 'ethers'
+
+// Make global Jest variable available
+declare var __DATABASE__: any
+declare var __INDRA__: any
+
+describe('State Channel', () => {
+  test('creates a state channel', async () => {
+    const sequelize = await dbConnect(__DATABASE__)
+    if (!__INDRA__.ethereumProvider || !__INDRA__.connextNode) {
+      console.error(`Could not run tests, Indra env vars not defined: ${__INDRA__}`)
+      // return true, not sure if we want to bail on tests here
+      expect(true).toBeTruthy()
+      return
+    }
+    const connextClient = await createStateChannel({
+      ...__INDRA__,
+      sequelize,
+      mnemonic: Wallet.createRandom().mnemonic,
+      logLevel: 3,
+    })
+    expect(connextClient.multisigAddress).toBeTruthy()
+  })
+})
