34e484df4e5aa40d31961ed514ee1f79f1ba19a4
Rahul Sethuram <rahul@Rahuls-MacBook-Pro.local>
2020-05-06 16:53:11 +0200

    Defaults are no longer exported

--- packages/common-ts/src/state-channels/index.ts
+++ packages/common-ts/src/state-channels/index.ts
@@ -1,13 +1,7 @@
 import * as connext from '@connext/client'
 import { Sequelize } from 'sequelize'
 import { ILogger, StoreTypes } from '@connext/types'
-import {
-  ConnextStore,
-  WrappedPostgresStorage,
-  DEFAULT_STORE_PREFIX,
-  DEFAULT_STORE_SEPARATOR,
-  DEFAULT_DATABASE_STORAGE_TABLE_NAME,
-} from '@connext/store'
+import { ConnextStore, WrappedPostgresStorage } from '@connext/store'
 import { Wallet } from 'ethers'
 
 interface StateChannelOptions {
@@ -23,18 +17,18 @@ interface StateChannelOptions {
 export const createStateChannel = async (options: StateChannelOptions) => {
   // Create Sequelize-based store
   const wrappedPostgresStorage = new WrappedPostgresStorage(
-    DEFAULT_STORE_PREFIX,
-    DEFAULT_STORE_SEPARATOR,
-    DEFAULT_DATABASE_STORAGE_TABLE_NAME,
+    undefined, // use default
+    undefined, // use default
+    undefined, // use default
     options.sequelize,
   )
   const store = new ConnextStore(StoreTypes.Postgres, { storage: wrappedPostgresStorage })
-  await wrappedPostgresStorage.syncModels();
+  await wrappedPostgresStorage.syncModels()
 
   // Create in-memory wallet from the mnemonic
   // We can replace this with just a private key if we don't want to instantiate
   // with a mnemonic
-  const wallet = Wallet.fromMnemonic(options.mnemonic);
+  const wallet = Wallet.fromMnemonic(options.mnemonic)
 
   return await connext.connect({
     ethProviderUrl: options.ethereumProvider,
