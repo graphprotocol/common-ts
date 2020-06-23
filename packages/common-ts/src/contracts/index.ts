7e51d5dad83298a34987a0ccff13ebec12b90e47
Ariel Barmat <abarmat@gmail.com>
2020-06-23 15:36:29 -0300

    Use contract factory bindings

--- packages/common-ts/src/contracts/index.ts
+++ packages/common-ts/src/contracts/index.ts
@@ -1,29 +1,39 @@
-import { Contract, providers } from 'ethers'
+import { providers } from 'ethers'
 import { Signer } from 'ethers'
 
 // Contract addresses
 const DEPLOYED_CONTRACTS = require('@graphprotocol/contracts/addresses.json')
 
 // Contract ABIs
-const CURATION_ABI = require('@graphprotocol/contracts/dist/abis/Curation.json')
-const DISPUTE_MANAGER_ABI = require('@graphprotocol/contracts/dist/abis/DisputeManager.json')
-const EPOCH_MANAGER_ABI = require('@graphprotocol/contracts/dist/abis/EpochManager.json')
-const GNS_ABI = require('@graphprotocol/contracts/dist/abis/GNS.json')
-const REWARDS_MANAGER_ABI = require('@graphprotocol/contracts/dist/abis/RewardsManager.json')
-const SERVICE_REGISTRY_ABI = require('@graphprotocol/contracts/dist/abis/ServiceRegistry.json')
-const STAKING_ABI = require('@graphprotocol/contracts/dist/abis/Staking.json')
-const GRAPH_TOKEN_ABI = require('@graphprotocol/contracts/dist/abis/GraphToken.json')
+import { Curation } from '@graphprotocol/contracts/dist/typechain/contracts/Curation'
+import { DisputeManager } from '@graphprotocol/contracts/dist/typechain/contracts/DisputeManager'
+import { EpochManager } from '@graphprotocol/contracts/dist/typechain/contracts/EpochManager'
+import { Gns } from '@graphprotocol/contracts/dist/typechain/contracts/Gns'
+import { RewardsManager } from '@graphprotocol/contracts/dist/typechain/contracts/RewardsManager'
+import { ServiceRegistry } from '@graphprotocol/contracts/dist/typechain/contracts/ServiceRegistry'
+import { Staking } from '@graphprotocol/contracts/dist/typechain/contracts/Staking'
+import { GraphToken } from '@graphprotocol/contracts/dist/typechain/contracts/GraphToken'
+
+// Contract factories
+import { CurationFactory } from '@graphprotocol/contracts/dist/typechain/contracts/CurationContract'
+import { DisputeManagerFactory } from '@graphprotocol/contracts/dist/typechain/contracts/DisputeManagerContract'
+import { EpochManagerFactory } from '@graphprotocol/contracts/dist/typechain/contracts/EpochManagerContract'
+import { GnsFactory } from '@graphprotocol/contracts/dist/typechain/contracts/GnsContract'
+import { RewardsManagerFactory } from '@graphprotocol/contracts/dist/typechain/contracts/RewardsManagerContract'
+import { ServiceRegistryFactory } from '@graphprotocol/contracts/dist/typechain/contracts/ServiceRegistryContract'
+import { StakingFactory } from '@graphprotocol/contracts/dist/typechain/contracts/StakingContract'
+import { GraphTokenFactory } from '@graphprotocol/contracts/dist/typechain/contracts/GraphTokenContract'
 
 // TODO: Remove when contracts repo has TypeChain with ethers v5 support.
 export interface NetworkContracts {
-  curation: any
-  disputeManager: any
-  epochManager: any
-  gns: any
-  rewardsManager: any
-  serviceRegistry: any
-  staking: any
-  token: any
+  curation: Curation
+  disputeManager: DisputeManager
+  epochManager: EpochManager
+  gns: Gns
+  rewardsManager: RewardsManager
+  serviceRegistry: ServiceRegistry
+  staking: Staking
+  token: GraphToken
 }
 
 export const connectContracts = async (
@@ -33,40 +43,30 @@ export const connectContracts = async (
   let deployedContracts = DEPLOYED_CONTRACTS[`${chainId}`]
 
   return {
-    curation: new Contract(
+    curation: CurationFactory.connect(
       deployedContracts.Curation.address,
-      CURATION_ABI,
       providerOrSigner,
     ),
-    disputeManager: new Contract(
+    disputeManager: DisputeManagerFactory.connect(
       deployedContracts.DisputeManager.address,
-      DISPUTE_MANAGER_ABI,
       providerOrSigner,
     ),
-    epochManager: new Contract(
+    epochManager: EpochManagerFactory.connect(
       deployedContracts.EpochManager.address,
-      EPOCH_MANAGER_ABI,
       providerOrSigner,
     ),
-    gns: new Contract(deployedContracts.GNS.address, GNS_ABI, providerOrSigner),
-    rewardsManager: new Contract(
+    gns: GnsFactory.connect(deployedContracts.GNS.address, providerOrSigner),
+    rewardsManager: RewardsManagerFactory.connect(
       deployedContracts.RewardsManager.address,
-      REWARDS_MANAGER_ABI,
       providerOrSigner,
     ),
-    serviceRegistry: new Contract(
+    serviceRegistry: ServiceRegistryFactory.connect(
       deployedContracts.ServiceRegistry.address,
-      SERVICE_REGISTRY_ABI,
       providerOrSigner,
     ),
-    staking: new Contract(
-      deployedContracts.Staking.address,
-      STAKING_ABI,
-      providerOrSigner,
-    ),
-    token: new Contract(
+    staking: StakingFactory.connect(deployedContracts.Staking.address, providerOrSigner),
+    token: GraphTokenFactory.connect(
       deployedContracts.GraphToken.address,
-      GRAPH_TOKEN_ABI,
       providerOrSigner,
     ),
   }
