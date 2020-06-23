30daa2ec16b657731ea7933da722a3791be16f5c
Jannis Pohlmann <jannis@thegraph.com>
2020-06-23 12:33:40 +0200

    Use TypeChain contract bindings

--- packages/common-ts/src/contracts/index.ts
+++ packages/common-ts/src/contracts/index.ts
@@ -5,66 +5,74 @@ import { Signer } from 'ethers'
 const DEPLOYED_CONTRACTS = require('@graphprotocol/contracts/addresses.json')
 
 // Contract ABIs
+import { Curation } from '@graphprotocol/contracts/dist/typechain/contracts/Curation'
 const CURATION_ABI = require('@graphprotocol/contracts/dist/abis/Curation.json')
+import { DisputeManager } from '@graphprotocol/contracts/dist/typechain/contracts/DisputeManager'
 const DISPUTE_MANAGER_ABI = require('@graphprotocol/contracts/dist/abis/DisputeManager.json')
+import { EpochManager } from '@graphprotocol/contracts/dist/typechain/contracts/EpochManager'
 const EPOCH_MANAGER_ABI = require('@graphprotocol/contracts/dist/abis/EpochManager.json')
+import { Gns } from '@graphprotocol/contracts/dist/typechain/contracts/Gns'
 const GNS_ABI = require('@graphprotocol/contracts/dist/abis/GNS.json')
+import { RewardsManager } from '@graphprotocol/contracts/dist/typechain/contracts/RewardsManager'
 const REWARDS_MANAGER_ABI = require('@graphprotocol/contracts/dist/abis/RewardsManager.json')
+import { ServiceRegistry } from '@graphprotocol/contracts/dist/typechain/contracts/ServiceRegistry'
 const SERVICE_REGISTRY_ABI = require('@graphprotocol/contracts/dist/abis/ServiceRegistry.json')
+import { Staking } from '@graphprotocol/contracts/dist/typechain/contracts/Staking'
 const STAKING_ABI = require('@graphprotocol/contracts/dist/abis/Staking.json')
+import { GraphToken } from '@graphprotocol/contracts/dist/typechain/contracts/GraphToken'
 const GRAPH_TOKEN_ABI = require('@graphprotocol/contracts/dist/abis/GraphToken.json')
 
 // TODO: Remove when contracts repo has TypeChain with ethers v5 support.
-export interface NetworkContracts {
-  curation: any
-  disputeManager: any
-  epochManager: any
-  gns: any
-  rewardsManager: any
-  serviceRegistry: any
-  staking: any
-  token: any
+export interface Contracts {
+  curation: Curation
+  disputeManager: DisputeManager
+  epochManager: EpochManager
+  gns: Gns
+  rewardsManager: RewardsManager
+  serviceRegistry: ServiceRegistry
+  staking: Staking
+  token: GraphToken
 }
 
-export const connectContracts = async (
+export const connect = async (
   providerOrSigner: providers.Provider | Signer,
   chainId: number,
-): Promise<NetworkContracts> => {
+): Promise<Contracts> => {
   let deployedContracts = DEPLOYED_CONTRACTS[`${chainId}`]
 
   return {
-    curation: new Contract(
+    curation: new Curation(
       deployedContracts.Curation.address,
       CURATION_ABI,
       providerOrSigner,
     ),
-    disputeManager: new Contract(
+    disputeManager: new DisputeManager(
       deployedContracts.DisputeManager.address,
       DISPUTE_MANAGER_ABI,
       providerOrSigner,
     ),
-    epochManager: new Contract(
+    epochManager: new EpochManager(
       deployedContracts.EpochManager.address,
       EPOCH_MANAGER_ABI,
       providerOrSigner,
     ),
-    gns: new Contract(deployedContracts.GNS.address, GNS_ABI, providerOrSigner),
-    rewardsManager: new Contract(
+    gns: new Gns(deployedContracts.GNS.address, GNS_ABI, providerOrSigner),
+    rewardsManager: new RewardsManager(
       deployedContracts.RewardsManager.address,
       REWARDS_MANAGER_ABI,
       providerOrSigner,
     ),
-    serviceRegistry: new Contract(
+    serviceRegistry: new ServiceRegistry(
       deployedContracts.ServiceRegistry.address,
       SERVICE_REGISTRY_ABI,
       providerOrSigner,
     ),
-    staking: new Contract(
+    staking: new Staking(
       deployedContracts.Staking.address,
       STAKING_ABI,
       providerOrSigner,
     ),
-    token: new Contract(
+    token: new GraphToken(
       deployedContracts.GraphToken.address,
       GRAPH_TOKEN_ABI,
       providerOrSigner,
