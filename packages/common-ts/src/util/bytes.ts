f0b7bb6ec60faf90c8b0e544add4a36c47471985
Jannis Pohlmann <jannis@edgeandnode.com>
2021-05-07 14:16:54 +0200

    Fix linter errors and warnings

--- packages/common-ts/src/util/bytes.ts
+++ packages/common-ts/src/util/bytes.ts
@@ -1,41 +1,41 @@
 // Writes bytes to a fixed size buffer with a capacity that is known upfront.
 export class BytesWriter {
-    private offset: number = 0;
-    private data: Uint8Array;
+  private offset = 0
+  private data: Uint8Array
 
-    constructor(capacity: number) {
-        this.data = new Uint8Array(capacity)
+  constructor(capacity: number) {
+    this.data = new Uint8Array(capacity)
+  }
+
+  // Write a hex string to this buffer
+  // The hex should start with 0x and have an even number of hexadecimal characters.
+  // This does no validation at all. Make sure the input is correct and that the
+  // writer has capacity.
+  writeHex(hex: string): void {
+    // Help the optimizer maybe by lifting local variables
+    let offset = this.offset
+    const data = this.data
+
+    // TODO: Performance
+    // There are no great answers in JS,
+    // but individual characters might be better. Need to profile.
+    let iRead = 2 // Skips 0x
+    while (iRead < hex.length) {
+      const num = hex.slice(iRead, iRead + 2)
+      data[offset++] = parseInt(num, 16)
+      iRead += 2
     }
+    this.offset = offset
+  }
 
-    // Write a hex string to this buffer
-    // The hex should start with 0x and have an even number of hexadecimal characters. 
-    // This does no validation at all. Make sure the input is correct and that the 
-    // writer has capacity.
-    writeHex(hex: string) {
-        // Help the optimizer maybe by lifting local variables
-        let offset = this.offset;
-        let data = this.data;
+  writeZeroes(bytes: number): void {
+    this.offset += bytes
+  }
 
-        // TODO: Performance
-        // There are no great answers in JS,
-        // but individual characters might be better. Need to profile.
-        let iRead = 2; // Skips 0x
-        while (iRead < hex.length) {
-            let num = hex.slice(iRead, iRead + 2);
-            data[offset++] = parseInt(num, 16);
-            iRead += 2;
-        }
-        this.offset = offset;
+  unwrap(): Uint8Array {
+    if (this.offset !== this.data.length) {
+      throw new Error('Did not write correct number of bytes')
     }
-
-    writeZeroes(bytes: number) {
-        this.offset += bytes;
-    }
-
-    unwrap(): Uint8Array {
-        if (this.offset !== this.data.length) {
-            throw new Error("Did not write correct number of bytes");
-        }
-        return this.data
-    }
+    return this.data
+  }
 }
