8ac1430126206f45d4b26323bcaa70024bd33ab0
Theo Butler <theodusbutler@gmail.com>
2023-11-14 10:22:25 -0500

    explicitly select address book

--- packages/common-ts/src/contracts/index.test.ts
+++ packages/common-ts/src/contracts/index.test.ts
@@ -1,5 +1,6 @@
 import { Signer } from 'ethers'
 import { connectContracts } from '.'
+import * as DEPLOYED_CONTRACTS from '@graphprotocol/contracts/addresses.json'
 
 jest.mock('ethers')
 
@@ -8,7 +9,7 @@ const mockSigner = jest.fn() as unknown as Signer
 describe('Contracts', () => {
   // Test for each supported protocol network
   test.each([1, 5, 42161, 421613])('Connect contracts [chainId: %p]', chainId => {
-    const contracts = connectContracts(mockSigner, chainId)
+    const contracts = connectContracts(mockSigner, chainId, DEPLOYED_CONTRACTS)
     expect(contracts).toBeDefined()
   })
 })
