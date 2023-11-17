1cbd4eac79ed09af7f422fc4d2314116006e70ce
Ford <ford@thegraph.com>
2023-11-16 16:15:26 -0800

    Add support for the Sepolia (L2) & Sepolia Arbitrum (L2) networks

--- packages/common-ts/src/contracts/index.test.ts
+++ packages/common-ts/src/contracts/index.test.ts
@@ -8,8 +8,18 @@ const mockSigner = jest.fn() as unknown as Signer
 
 describe('Contracts', () => {
   // Test for each supported protocol network
-  test.each([1, 5, 42161, 421613])('Connect contracts [chainId: %p]', chainId => {
-    const contracts = connectContracts(mockSigner, chainId, DEPLOYED_CONTRACTS)
-    expect(contracts).toBeDefined()
-  })
+  test.each([1, 5, 42161, 421613, 421614, 11155111])(
+    'Connect contracts with explicit addressBook provided [chainId: %p]',
+    chainId => {
+      const contracts = connectContracts(mockSigner, chainId, DEPLOYED_CONTRACTS)
+      expect(contracts).toBeDefined()
+    },
+  )
+  test.each([1, 5, 42161, 421613, 421614, 11155111])(
+    'Connect contracts without explicit addressBook provided [chainId: %p]',
+    chainId => {
+      const contracts = connectContracts(mockSigner, chainId, undefined)
+      expect(contracts).toBeDefined()
+    },
+  )
 })
