cb476a9a084e902d31d9e82454fc733b5545b2df
Miguel de Elias <miguel@edgeandnode.com>
2025-05-16 15:54:45 -0300

    fix: export commify
    Signed-off-by: Tomás Migone <tomas@edgeandnode.com>

--- packages/common-ts/src/util/commify.ts
+++ packages/common-ts/src/util/commify.ts
@@ -1,14 +1,14 @@
-import "ethers"
+import 'ethers'
 
-function commify(value: string): string {
-  const match = value.match(/^(-?)([0-9]*)(\.?)([0-9]*)$/);
+export function commify(value: string): string {
+  const match = value.match(/^(-?)([0-9]*)(\.?)([0-9]*)$/)
   if (!match || (!match[2] && !match[4])) {
-    throw new Error(`bad formatted number: ${ JSON.stringify(value) }`);
+    throw new Error(`bad formatted number: ${JSON.stringify(value)}`)
   }
 
-  const neg = match[1];
-  const whole = BigInt(match[2] || 0).toLocaleString("en-us");
-  const frac = match[4] ? (match[4].match(/^(.*?)0*$/)?.[1] || "0") : "0";
+  const neg = match[1]
+  const whole = BigInt(match[2] || 0).toLocaleString('en-us')
+  const frac = match[4] ? match[4].match(/^(.*?)0*$/)?.[1] || '0' : '0'
 
-  return `${ neg }${ whole }.${ frac }`;
+  return `${neg}${whole}.${frac}`
 }
