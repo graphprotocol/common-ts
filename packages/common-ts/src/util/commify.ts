e1d7ad0021e5c643f48c83f1c5f26247cd5b8a99
Miguel de Elias <miguel@edgeandnode.com>
2025-05-16 15:37:37 -0300

    chore: add commify
    Signed-off-by: Tomás Migone <tomas@edgeandnode.com>

---
+++ packages/common-ts/src/util/commify.ts
@@ -0,0 +1,14 @@
+import "ethers"
+
+function commify(value: string): string {
+  const match = value.match(/^(-?)([0-9]*)(\.?)([0-9]*)$/);
+  if (!match || (!match[2] && !match[4])) {
+    throw new Error(`bad formatted number: ${ JSON.stringify(value) }`);
+  }
+
+  const neg = match[1];
+  const whole = BigInt(match[2] || 0).toLocaleString("en-us");
+  const frac = match[4] ? (match[4].match(/^(.*?)0*$/)?.[1] || "0") : "0";
+
+  return `${ neg }${ whole }.${ frac }`;
+}
